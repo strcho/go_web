e189750496e2907884a66572000976bd39697c3f
Orcs <972576060@qq.com>
2021-11-10 16:46:50 +0800

    项目清理

--- routes/k8s_server/__init__.py
+++ routes/k8s_server/__init__.py
@@ -1,6 +1,7 @@
 from routes.k8s_server.view import K8sHandler
 
 
+# /account
 urls = [
     (r'/heartbeat/k8s', K8sHandler),  # 心跳探测接口需要保留
 ]
--- routes/wallet/__init__.py
+++ routes/wallet/__init__.py
@@ -1,6 +1,7 @@
 from routes.wallet.view import WalletHandle
 
 
+# /account/user
 urls = [
         (r'/wallet', WalletHandle),  # 用户钱包
 ]
