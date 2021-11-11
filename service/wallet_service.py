3593b65109121ad2da228271e1e87ffa0b1ffc5e
Orcs <972576060@qq.com>
2021-11-11 10:50:52 +0800

    fix

--- service/wallet_service.py
+++ service/wallet_service.py
@@ -14,9 +14,8 @@ class WalletService(MBService):
 
     def query_one(self, valid_data):
         pin_id, _ = valid_data
-        params = {"pin_id": pin_id}
         try:
-            user_wallet = dao_session.session().query(UserWallet).filter_by(**params).first()
+            user_wallet = dao_session.session().query(UserWallet).filter_by(pin_id=pin_id).first()
         except Exception as e:
             dao_session.session().rollback()
             logger.error("query user wallet is error: {}".format(e))
@@ -25,14 +24,60 @@ class WalletService(MBService):
 
     def insert_one(self, vaild_data):
 
+        data = dict(
+            id=ID_Worker(),
+            tenant_id="",
+            created_pin=None,
+            updated_pin=None
+        )
+        user_wallet = UserWallet(**data)
+        dao_session.session().add(user_wallet)
         try:
-            data = dict(
-                id=ID_Worker(),
-                tenant_id="",
-                created_pin=None,
-                updated_pin=None
-            )
+            dao_session.commit()
+            return True
+
         except Exception as e:
             dao_session.session().rollback()
             logger.error("insert user wallet is error: {}".format(e))
             logger.exception(e)
+            return False
+
+    def update_one(self, vaild_data):
+
+        pin_id, _ = vaild_data
+        data = dict()
+        params = dict()
+        # 更新余额考虑使用 update({"balance": UserWallet.balance - change})
+        dao_session.session().query(UserWallet)\
+            .filter_by(pin_id=pin_id).update(params)\
+            .with_for_update(read=True, nowait=False, of=None)
+        try:
+            dao_session.session().commit()
+            return True
+        except Exception as e:
+            dao_session.session().rollback()
+            logger.error("update user wallet is error: {}".format(e))
+            logger.exception(e)
+            return False
+
+    def query_list(self, valid_data, enable=2):
+
+        pin_ids, _ = valid_data
+
+        user_wallets = dao_session.session().query(UserWallet).filter(UserWallet.pin_id.in_(pin_ids)).all()
+        data_list = []
+        count = len(data_list)
+        try:
+            for user_wallet in user_wallets:
+                data_list.append(
+                    {
+
+                    }
+                )
+        except Exception as e:
+            dao_session.session().rollback()
+            logger.error("")
+            logger.exception(e)
+            return False
+
+        return data_list, count
