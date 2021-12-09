01254b3cac1a1d9f8cb9e9042c09fb7dc023d19d
Orcs <972576060@qq.com>
2021-12-09 14:31:18 +0800

    fix

--- routes/wallet/serializers.py
+++ routes/wallet/serializers.py
@@ -65,4 +65,4 @@ class DeductionBalanceDeserializer(ReqBaseDeserializer):
     """
 
     pin = fields.String(required=True, description="用户标识")
-    change_recharge = fields.Integer(required=True, description="扣减的余额*100")
+    deduction_amount = fields.Integer(required=True, description="扣减的余额*100")
