852defbd60be52f9f847b42b3af39d7f5b2042cf
Orcs <972576060@qq.com>
2022-02-10 14:50:12 +0800

    fix

--- internal/marketing_api.py
+++ internal/marketing_api.py
@@ -16,12 +16,12 @@ def riding_card_info(jsonData):
     pass
 
 
-@nacosClient.customRequestClient(method="POST", url="/ebike_marketing/riding_config/platform/favorable_card", )  # TODO
+@nacosClient.customRequestClient(method="POST", url="/ebike_marketing/favorable_config/platform/favorable_card", )
 def favorable_card_info(jsonData):
     pass
 
 
-@nacosClient.customRequestClient(method="POST", url="/ebike_marketing/riding_config/platform/favorable_card", )
+@nacosClient.customRequestClient(method="POST", url="/ebike_marketing/deposit_config/platform/deposit_card", )
 def deposit_card_info(jsonData):
     pass
 
@@ -43,7 +43,7 @@ class MarketingApi:
 
     @staticmethod
     def get_favorable_card_info(config_id: int, command_context: dict):
-        param = {"riding_card_config_id": config_id, 'commandContext': command_context}
+        param = {"card_id": config_id, 'commandContext': command_context}
         card_res = favorable_card_info(param)
         card_res_data = json.loads(card_res)
         if not card_res_data.get("success"):
@@ -53,9 +53,10 @@ class MarketingApi:
 
     @staticmethod
     def get_deposit_card_info(config_id: int, command_context: dict):
-        param = {"riding_card_config_id": config_id, 'commandContext': command_context}
+        param = {"deposit_card_id": str(config_id), 'commandContext': command_context}
         card_res = deposit_card_info(param)
         card_res_data = json.loads(card_res)
+        print(card_res_data)
         if not card_res_data.get("success"):
             raise MbException("营销活动服务调用失败")
 
