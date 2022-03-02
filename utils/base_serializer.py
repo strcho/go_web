abb4fc46d5497b2bcffb4635ff1b503d95d6cf55
Orcs <972576060@qq.com>
2022-03-02 18:00:36 +0800

    fix

--- utils/base_serializer.py
+++ utils/base_serializer.py
@@ -9,12 +9,12 @@ from mbutils.mb_handler import ContextDeserializer
 
 class CommandContext(ContextDeserializer):
     # tenant_id = fields.String(required=False, description="租户ID", data_key='tenantId')
-    tenantId = fields.String(required=False, description="租户ID")
-    traceId = fields.String(required=False, description="链路追踪ID")
-    pin = fields.String(required=False, description="用户PIN")
-    ip = fields.String(required=False, description="ip地址")
-    source = fields.String(required=False, description="系统标识")
-    stressTesting = fields.Bool(required=False, description="false  压测标识)true 压测请求 false 正常请求")
+    tenantId = fields.String(required=True, description="租户ID")
+    traceId = fields.String(allow_none=True, description="链路追踪ID")
+    pin = fields.String(allow_none=True, description="用户PIN")
+    ip = fields.String(allow_none=True, description="ip地址")
+    source = fields.String(allow_none=True, description="系统标识")
+    stressTesting = fields.Bool(allow_none=True, description="false  压测标识)true 压测请求 false 正常请求")
 
 
 class ReqBaseDeserializer(ContextDeserializer):
